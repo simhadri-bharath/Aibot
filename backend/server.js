b45ab7be23a5653fd11e58871bdab1006561e923
simhadri-bharath <simhadribharath2004@gmail.com>
2025-03-14 21:01:46 +0530

    add socket.io integration in backend

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,22 @@ dotenv.config();  // Load environment variables from .env file
 
 import http from 'http';
 import app from './app.js';
+import {Server} from 'socket.io';
 
-const  port =process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-const server=http.createServer(app);
+const server = http.createServer(app);
 
-server.listen(port,()=>{    
+const io = new Server(server);
+io.on('connection', socket => {
+
+    console.log('a user connected');x
+
+    socket.on('event', data => { /* … */ });
+    socket.on('disconnect', () => { /* … */ });
+});
+
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 }
 );
